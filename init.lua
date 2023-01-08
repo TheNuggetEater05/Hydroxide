464557f02d6f67e82d9d8aa0fe2c26ed6b7952ce
TheNuggetEater05 <63414528+TheNuggetEater05@users.noreply.github.com>
2023-01-08 14:03:06 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ if oh then
 end
 
 local web = true
-local user = "IHaxU" -- change if you're using a fork
+local user = "TheNuggetEater05" -- change if you're using a fork
 local branch = "revision"
 local importCache = {}
 
